cc321244ac054706dda5ea44a76288ef1aa35ddf
lksa4e <55073084+lksa4e@users.noreply.github.com>
2020-10-12 12:42:13 +0900

    Add files via upload

---
+++ src/parallelTest/CrossBrowserScript.java
@@ -0,0 +1,66 @@
+package parallelTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class CrossBrowserScript {
+
+	WebDriver driver;
+	
+	/**
+	 * This function will execute before each Test tag in testng.xml
+	 * @param browser
+	 * @throws Exception
+	 */
+	@BeforeTest
+	@Parameters("browser")
+	public void setup(String browser) throws Exception{
+		//Check if parameter passed from TestNG is 'firefox'
+		if(browser.equalsIgnoreCase("firefox")){
+		//create firefox instance
+			System.setProperty("webdriver.firefox.marionette", ".\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		//Check if parameter passed as 'chrome'
+		else if(browser.equalsIgnoreCase("chrome")){
+			//set path to chromedriver.exe
+			System.setProperty("webdriver.chrome.driver",".\\chromedriver.exe");
+			//create chrome instance
+			driver = new ChromeDriver();
+		}
+		//Check if parameter passed as 'Edge'
+				else if(browser.equalsIgnoreCase("Edge")){
+					//set path to Edge driver.exe
+					System.setProperty("webdriver.edge.driver",".\\MicrosoftWebDriver.exe");
+					//create Edge instance
+					driver = new EdgeDriver();
+				}
+		else{
+			//If no browser passed throw exception
+			throw new Exception("Browser is not correct");
+		}
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void testParameterWithXML() throws InterruptedException{
+		driver.get("http://3.35.140.212//cloud.jsp");
+		//Find user name
+		WebElement userName = driver.findElement(By.name("inputid"));
+		//Fill user name
+		userName.sendKeys("KKJ");
+		//Find password
+		WebElement password = driver.findElement(By.name("inputpassword"));
+		//Fill password
+		password.sendKeys("Passw0rd!");
+	}
+}
